eee85886ff0c78a5d51821d4d475edbf6d918425
Rynx007 <rynx007@gmail.com>
2024-10-06 22:09:20 +0545

    Added Package

--- client/src/assets/partials/Navbar/HeaderMegaMenu.jsx
+++ client/src/assets/partials/Navbar/HeaderMegaMenu.jsx
@@ -20,6 +20,7 @@ import {
   useMantineTheme,
   Avatar,
 } from "@mantine/core";
+import { SomeIcon } from "@tabler/icons-react";
 
 import { useDisclosure } from "@mantine/hooks";
 import {
@@ -80,18 +81,18 @@ export function HeaderMegaMenu() {
 
   const links = mockdata.map((item) => (
     <UnstyledButton className={classes.subLink} key={item.title}>
-      <Group wrap='nowrap' align='flex-start'>
-        <ThemeIcon size={34} variant='default' radius='md'>
+      <Group wrap="nowrap" align="flex-start">
+        <ThemeIcon size={34} variant="default" radius="md">
           <item.icon
             style={{ width: rem(22), height: rem(22) }}
             color={theme.colors.blue[6]}
           />
         </ThemeIcon>
         <div>
-          <Text size='sm' fw={500}>
+          <Text size="sm" fw={500}>
             {item.title}
           </Text>
-          <Text size='xs' c='dimmed'>
+          <Text size="xs" c="dimmed">
             {item.description}
           </Text>
         </div>
@@ -108,72 +109,80 @@ export function HeaderMegaMenu() {
   };
 
   return (
-    <Box className='pt-2 pb-1 border-b-2 border-gray-200'>
+    <Box className="pt-2 pb-1 border-b-2 border-gray-200">
       <header className={classes.header}>
-        <Group justify='space-between' h='100%'>
-          <h2 className='font-sans font-bold text-xl text-red-500 transform transition-transform duration-300 hover:scale-125 cursor-pointer'>
+        <Group justify="space-between" h="100%">
+          <h2 className="font-sans font-bold text-xl text-red-500 transform transition-transform duration-300 hover:scale-125 cursor-pointer">
             <Link to={"/"}>MernRestro</Link>
           </h2>
 
-          <Group h='100%' gap={0} visibleFrom='sm'>
+          <Group h="100%" gap={0} visibleFrom="sm">
             <Link
               to={"/home"}
-              className='text-lg font-sans font-bold text-red-600 transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2'>
+              className="text-lg font-sans font-bold text-red-600 transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2"
+            >
               Home
             </Link>
             <Link
               to={"/aboutus"}
-              className='text-lg font-sans font-bold text-red-600 transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2'>
+              className="text-lg font-sans font-bold text-red-600 transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2"
+            >
               About Us
             </Link>
             <Link
               to={"/menu"}
-              className='text-lg font-sans font-bold text-red-600 transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2'>
+              className="text-lg font-sans font-bold text-red-600 transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2"
+            >
               Menu
             </Link>
             <Link
               to={"/contactus"}
-              className='text-lg font-sans font-bold text-red-600 transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2'>
+              className="text-lg font-sans font-bold text-red-600 transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2"
+            >
               Contact Us
             </Link>
             <Link
               to={"/favourite"}
-              className='text-3xl font-sans font-extrabold text-red-600 transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2'>
+              className="text-3xl font-sans font-extrabold text-red-600 transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2"
+            >
               <AiOutlineHeart />
             </Link>
           </Group>
 
-          <Group visibleFrom='sm'>
+          <Group visibleFrom="sm">
             <Link
               to={"/delivery"}
-              className='text-4xl font-sans font-extrabold text-gray-700 transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2'>
+              className="text-4xl font-sans font-extrabold text-gray-700 transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2"
+            >
               <MdDeliveryDining />
             </Link>
             <Link
               to={"/order"}
-              className='text-2xl font-sans font-extrabold text-gray-700 transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2'>
+              className="text-2xl font-sans font-extrabold text-gray-700 transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2"
+            >
               <FaClipboardCheck />
             </Link>
             {reduxToken ? (
-              <div className='transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2'>
-                <button className='pt-2'>
-                  <Avatar variant='white' radius='lg' size='lg' src='' />
+              <div className="transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2">
+                <button className="pt-2">
+                  <Avatar variant="white" radius="lg" size="lg" src="" />
                 </button>
               </div>
             ) : (
-              <button className='bg-red-600 text-white border-red-100 border-2 font-bold py-1 px-4 rounded-xl transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2'>
+              <button className="bg-red-600 text-white border-red-100 border-2 font-bold py-1 px-4 rounded-xl transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2">
                 <Link to={"/login"}>Login</Link>
               </button>
             )}
 
             {reduxToken ? (
               <button
-                className='py-1 px-4 rounded-xl border-gray-200 border-2 bg-white text-black font-bold transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2'
-                onClick={handleLogout}>
+                className="py-1 px-4 rounded-xl border-gray-200 border-2 bg-white text-black font-bold transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2"
+                onClick={handleLogout}
+              >
                 Logout
               </button>
             ) : (
-              <button className='py-1 px-4 rounded-xl border-gray-200 border-2 bg-white text-black font-bold transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2'>
+              <button className="py-1 px-4 rounded-xl border-gray-200 border-2 bg-white text-black font-bold transform transition-transform duration-300 hover:scale-110 cursor-pointer mx-2">
                 <Link to={"/signup"}>SignUp</Link>
               </button>
             )}
@@ -182,71 +191,77 @@ export function HeaderMegaMenu() {
           <Burger
             opened={drawerOpened}
             onClick={toggleDrawer}
-            hiddenFrom='sm'
+            hiddenFrom="sm"
           />
         </Group>
       </header>
 
       <Drawer
-        className='font-sans font-extrabold text-red-500   '
+        className="font-sans font-extrabold text-red-500   "
         opened={drawerOpened}
         onClose={closeDrawer}
-        size='100%'
-        padding='md'
-        title='MernRestro'
-        hiddenFrom='sm'
-        zIndex={1000000}>
-        <ScrollArea h={`calc(100vh - ${rem(80)})`} mx='-md' className="font-sans font-extrabold text-red-500 ">
-          <Divider my='sm' />
+        size="100%"
+        padding="md"
+        title="MernRestro"
+        hiddenFrom="sm"
+        zIndex={1000000}
+      >
+        <ScrollArea
+          h={`calc(100vh - ${rem(80)})`}
+          mx="-md"
+          className="font-sans font-extrabold text-red-500 "
+        >
+          <Divider my="sm" />
 
           <div className={classes.link} onClick={closeDrawer}>
-            <span className='font-sans text-2xl pb-2 font-bold text-red-600 transform transition-colors duration-300 hover:scale-110 cursor-pointer'>
+            <span className="font-sans text-2xl pb-2 font-bold text-red-600 transform transition-colors duration-300 hover:scale-110 cursor-pointer">
               <Link to={"/menu"}>Menu</Link>
             </span>
           </div>
           <Collapse in={linksOpened}>{links}</Collapse>
           <div className={classes.link} onClick={closeDrawer}>
-            <span className='font-sans text-2xl pb-2 font-bold text-red-600 transform transition-colors duration-300 hover:scale-110 cursor-pointer'>
+            <span className="font-sans text-2xl pb-2 font-bold text-red-600 transform transition-colors duration-300 hover:scale-110 cursor-pointer">
               <Link to={"/aboutus"}>About Us</Link>
             </span>
           </div>
           <div className={classes.link} onClick={closeDrawer}>
-            <span className='font-sans font-bold text-2xl pb-2 text-red-600 transform transition-colors duration-300 hover:scale-110 cursor-pointer'>
+            <span className="font-sans font-bold text-2xl pb-2 text-red-600 transform transition-colors duration-300 hover:scale-110 cursor-pointer">
               <Link to={"/contactus"}>Contact Us</Link>
             </span>
           </div>
           <div className={classes.link} onClick={closeDrawer}>
             <Link
               to={"/favourite"}
-              className='flex justify-center transform transition-colors duration-300 hover:scale-110 cursor-pointer'
-              onClick={closeDrawer}>
-              <span className='font-sans font-bold text-2xl pb-2 text-red-600 transform transition-colors duration-300 hover:scale-110 cursor-pointer'>
+              className="flex justify-center transform transition-colors duration-300 hover:scale-110 cursor-pointer"
+              onClick={closeDrawer}
+            >
+              <span className="font-sans font-bold text-2xl pb-2 text-red-600 transform transition-colors duration-300 hover:scale-110 cursor-pointer">
                 <Link to={"/favourite"}>Favourite</Link>
               </span>
-              <span className='text-3xl ml-1 text-red-600 font-semibold transform transition-transform duration-300 hover:scale-110 cursor-pointer'>
+              <span className="text-3xl ml-1 text-red-600 font-semibold transform transition-transform duration-300 hover:scale-110 cursor-pointer">
                 <AiOutlineHeart />
               </span>
             </Link>
           </div>
           <div className={classes.link} onClick={closeDrawer}>
-            <Link to={"/order"} className='flex justify-center'>
-              <span className='font-sans text-xl font-bold transform transition-transform duration-300 hover:scale-110 cursor-pointer'>
+            <Link to={"/order"} className="flex justify-center">
+              <span className="font-sans text-xl font-bold transform transition-transform duration-300 hover:scale-110 cursor-pointer">
                 Your Order
               </span>
-              <span className='text-3xl ml-1 text-gray-700 font-extrabold'>
+              <span className="text-3xl ml-1 text-gray-700 font-extrabold">
                 <MdDeliveryDining />
               </span>
             </Link>
           </div>
-          <Divider my='sm' />
+          <Divider my="sm" />
 
-          <Group justify='center' grow pb='xl' px='md'>
-            <button className='bg-red-600 text-white border-red-100 border-2 font-bold py-1 px-4 rounded-xl transform transition-transform duration-300 hover:scale-110 cursor-pointer'>
+          <Group justify="center" grow pb="xl" px="md">
+            <button className="bg-red-600 text-white border-red-100 border-2 font-bold py-1 px-4 rounded-xl transform transition-transform duration-300 hover:scale-110 cursor-pointer">
               <Link to={"/login"} onClick={closeDrawer}>
                 Login
               </Link>
             </button>
-            <button className='py-1 px-4 rounded-xl border-gray-200 border-2 bg-white text-black font-bold transform transition-transform duration-300 hover:scale-110 cursor-pointer'>
+            <button className="py-1 px-4 rounded-xl border-gray-200 border-2 bg-white text-black font-bold transform transition-transform duration-300 hover:scale-110 cursor-pointer">
               <Link to={"/signup"} onClick={closeDrawer}>
                 SignUp
               </Link>
